aaa73616314357ceb9e78dcfaac9a7b6a6535e1f
Sigrid Marita Kvamme <sigridkvamme@gmail.com>
2019-12-10 20:21:36 +0900

    feat: Extract app blocking feature into this app
    * Also improve notification to actually have some content. It now has an expandable version wwhich lists the usage of the apps you have used recently. So it shows only the information you are interested in and nothing more

--- app/src/main/java/com/kvamme/goldenpegasusius/MainActivity.kt
+++ app/src/main/java/com/kvamme/goldenpegasusius/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.kvamme.goldenpegasusius
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.kvamme.goldenpegasusius.ui.main.MainFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
+
+object FragmentToLoad {
+    val MAIN = 0
+    val SETTINGS = 1
+}
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val gson = Gson()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_main,
+                R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        when (intent.extras?.getInt("frgToLoad")) {
+            0 -> navController.navigate(R.id.navigation_main)
+            1 -> navController.navigate(R.id.navigation_settings)
         }
     }
 
---
+++ app/src/main/java/com/kvamme/goldenpegasusius/ui/main/RestrictedAppListAdapter.kt
@@ -0,0 +1,94 @@
+package com.kvamme.goldenpegasusius.ui.main
+
+import android.graphics.Color
+import android.os.CountDownTimer
+import android.os.SystemClock
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Chronometer
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kvamme.goldenpegasusius.R
+
+class RestrictedAppListAdapter(
+    private var appList: List<AppDisplayListItem>
+) :
+    RecyclerView.Adapter<RestrictedAppListAdapter.AppViewHolder>() {
+
+//    private var appList = blockedAppList
+
+    class AppViewHolder(appListItem: View) : RecyclerView.ViewHolder(appListItem) {
+        var appName: TextView = appListItem.findViewById(R.id.appItemName)
+        var appTime: Chronometer = appListItem.findViewById(R.id.appItemTime)
+        var appIcon: ImageView = appListItem.findViewById(R.id.appItemIcon)
+        var blockedText: TextView = appListItem.findViewById(R.id.appItemBlockedText)
+        var totalUsage: TextView = appListItem.findViewById(R.id.appItemTotalUsageTime)
+        var remainingUsage: TextView = appListItem.findViewById(R.id.appItemRemainingUsage)
+    }
+
+    // Create new app blocking views invoked by layout manager
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppViewHolder {
+        val appListItem = LayoutInflater.from(parent.context)
+            .inflate(R.layout.app_blocking_item, parent, false)
+        // Layout settings for each list item
+        appListItem.setPadding(20, 10, 10, 10)
+        return AppViewHolder(appListItem)
+    }
+
+    // Replace contents of view invoked by layout manager
+    override fun onBindViewHolder(holder: AppViewHolder, position: Int) {
+        holder.appName.text = appList[position].displayName
+        val finishTimeStamp = appList[position].blockTimeStamp
+        holder.appIcon.setImageDrawable(appList[position].icon)
+
+        val isAppBlocked = finishTimeStamp != null
+        if (isAppBlocked) {
+            holder.blockedText.visibility = View.VISIBLE
+            holder.appTime.visibility = View.VISIBLE
+            holder.totalUsage.visibility = View.INVISIBLE
+            holder.remainingUsage.visibility = View.GONE
+
+            if (finishTimeStamp != null && System.currentTimeMillis() < finishTimeStamp) {
+                getBlockCountdown(finishTimeStamp, holder.appTime, holder).start()
+            } else if (finishTimeStamp != null && finishTimeStamp <= System.currentTimeMillis()) {
+                holder.appTime.text = "00:00"
+                holder.appTime.setTextColor(Color.parseColor("#8bc34a"))
+            }
+        } else {
+            holder.totalUsage.text = appList[position].todayUsageString
+            holder.remainingUsage.text = appList[position].remainingUsage
+        }
+    }
+
+    override fun getItemCount(): Int = appList.size
+
+    fun setAppList(newAppList: List<AppDisplayListItem>) {
+        this.appList = newAppList
+        notifyDataSetChanged()
+    }
+
+
+    // Get countdown timer for each app in adapter list
+    private fun getBlockCountdown(
+        countDownFromTime: Long,
+        chrono: Chronometer,
+        holder: AppViewHolder
+    ): CountDownTimer {
+        val msToFinish = countDownFromTime - System.currentTimeMillis()
+        chrono.base = SystemClock.elapsedRealtime() + msToFinish
+        chrono.start()
+
+        return object : CountDownTimer(msToFinish, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                holder.appTime.text = "00:00"
+                holder.appTime.setTextColor(Color.parseColor("#8bc34a"))
+                chrono.stop()
+            }
+        }
+    }
+}
